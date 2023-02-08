73ab821483d1509e99ff0227ce29ac5b4133882b
ut <utkarshrm568@gmail.com>
2023-02-08 15:11:35 +0530

    idk

--- type.js
+++ type.js
@@ -7,12 +7,9 @@ var aText = new Array(
   '[    0.606518] Interests: NULL',
   '[    0.606934] LinkedIn Profile: <a href="https://www.linkedin.com/in/utkar5hm/">/Utkar5hM</a>',
   '[    0.607323] Github Profile: <a href="https://github.com/Utkar5hM">@Utkar5hM</a>',
-  '[    0.607774] Instagram Profile: <a href="https://www.instagram.com/utkar5hm/">@Utkar5hM</a>',
-  '[    0.608222] Facebook Profile: <a href="https://www.facebook.com/utkar5hM">/Utkar5hM</a>',
+  '[    0.607774] Twitter Profile: <a href="https://twitter.com/Utkar5hm">@Utkar5hM</a>',
   '[    0.608340] Youtube Channel: <a href="https://www.youtube.com/channel/UCTA5vSw4kZs04TqCw6nDAMg">Utkarsh M</a>',
-  '[    0.608736] TryhackMe Profile: <a href="https://tryhackme.com/p/Utkar5hM">/Utkar5hM</a>',
-  '[    0.608746] Medium Profile: <a href="https://medium.com/@utkar5hm">/Utkar5hM</a>',
-  '[    0.609248] Resume: <a href="https://drive.google.com/drive/folders/1h34oaLfqFc5VKA0_lWYup3R-gTgGr_iv?usp=sharing">Gdrive Link</a>',
+  '[    0.608222] Blog Site: <a href="https://blog.utkar5hm.tk/">blog.Utkar5hM.tk</a>',
 );
 var iSpeed = 1; // time delay of print out
 var iIndex = 0; // start printing array at this posision
