243ca4c6e696499cad200d6f7c62eb9aefc3cb0d
Utkarsh Mahajan <utkarshrm568@gmail.com>
2024-07-20 04:44:46 +0530

    test
    Signed-off-by: Utkarsh Mahajan <utkarshrm568@gmail.com>

--- type.js
+++ type.js
@@ -1,15 +1,11 @@
 // set up text to print, each item in array is new line
 var aText = new Array(
   '[    0.604303] Name: Utkarsh M',
-  '[    0.605392] Date Of Birth: 25th October 2001',
-  '[    0.605752] Institute: National Institute of Technology Karnataka, Surathkal',
-  '[    0.606097] Degree: Btech in Electronics and Communication Engineering',
-  '[    0.606518] Interests: NULL',
+  '[    0.606518] Interests: Many (-.-) Zzz…',
   '[    0.606934] LinkedIn Profile: <a href="https://www.linkedin.com/in/utkar5hm/">/Utkar5hM</a>',
   '[    0.607323] Github Profile: <a href="https://github.com/Utkar5hM">@Utkar5hM</a>',
   '[    0.607774] Twitter Profile: <a href="https://twitter.com/Utkar5hm">@Utkar5hM</a>',
-  '[    0.608340] Youtube Channel: <a href="https://www.youtube.com/channel/UCTA5vSw4kZs04TqCw6nDAMg">Utkarsh M</a>',
-  '[    0.608222] Blog Site: <a href="https://blog.utkar5hm.tk/">blog.Utkar5hM.tk</a>',
+  '[    0.608222] Blogs: <a href="https://utkar5hm.github.io">utkar5hm.github.io</a>',
 );
 var iSpeed = 1; // time delay of print out
 var iIndex = 0; // start printing array at this posision
